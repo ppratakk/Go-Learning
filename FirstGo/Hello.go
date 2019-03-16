016735ff7dd3007ed251f269f315e40f18d52d12
ppratakk <kla@gettgo.com>
2019-03-16 10:09:39 +0700

    initial commit

---
+++ FirstGo/Hello.go
@@ -0,0 +1,14 @@
+//package declaration.
+package main
+
+/*
+	Package fmt implements formatted I/O with functions analogous to C's printf and scanf.
+	The format 'verbs' are derived from C's but are simpler.
+	https://golang.org/pkg/fmt/
+	Note: can use 'goimports -w {filename}.go' for automatic import package.
+*/
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
